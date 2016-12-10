2abdc0b23a4d7188351e70baefa651f84a1de55b
Joseph Fitzpatrick <jfitzpa1@tcd.ie>
2016-12-10 17:52:02 +0000

    Finished Database Design

--- GrainStore.sql
+++ GrainStore.sql
@@ -1,8 +1,6 @@
 DROP TABLE load;
-
 DROP TABLE trailer;
 DROP TABLE farmer;
-
 DROP TABLE employee_roles;
 DROP TABLE employee;
 DROP TABLE roles;
@@ -26,7 +24,7 @@ CREATE TABLE branch(
 	streetName varchar(255) NOT NULL,
 	town varchar(255) NOT NULL,
 	county varchar(255) NOT NULL,
-	storageCapacity int NOT NULL
+	storageCapacity int NOT NULL,
 );
 ALTER TABLE branch ADD ( CONSTRAINT branch_pk PRIMARY KEY(branch_id));	
 CREATE SEQUENCE branch_seq 
@@ -47,6 +45,7 @@ CREATE TABLE farmer (
 	farmer_id int NOT NULL,
 	name varchar(255), 
 	streetName varchar(255) NOT NULL,
+	phoneNumber int NOT NULL,
 	town varchar(255) NOT NULL,
 	county varchar(255) NOT NULL	
 );
@@ -95,14 +94,17 @@ CREATE TABLE employee (
 	county varchar(255) NOT NULL,
 	salary int NOT NULL,
 	phoneNumber int NOT NULL,
+	ppsNum int NOT NULL,
 	branch_id int NOT NULL
 );
 ALTER TABLE employee ADD ( CONSTRAINT employee_pk1 PRIMARY KEY(employee_id));
 ALTER TABLE employee ADD ( CONSTRAINT emp_branch FOREIGN KEY (branch_id) REFERENCES branch(branch_id));
+ALTER TABLE employee ADD ( CONSTRAINT psp_check_min CHECK (ppsNum > 99999999));
+ALTER TABLE employee ADD ( CONSTRAINT psp_check_max CHECK (ppsNum < 1000000000));
 	
 CREATE SEQUENCE employee_seq START WITH 1;
 
-CREATE OR REPLACE TRIGGER employee_bi
+CREATE OR REPLACE TRIGGER employee_id_auto_increment
 BEFORE INSERT ON employee
 FOR EACH ROW
 BEGIN 
@@ -111,6 +113,7 @@ BEGIN
 	FROM dual;
 END;
 /
+
 
 CREATE TABLE roles(
 	roles_id int NOT NULL,
@@ -137,6 +140,20 @@ CREATE TABLE employee_roles(
 ALTER TABLE employee_roles ADD ( CONSTRAINT employee_roles_fk2 FOREIGN KEY(employee_id) REFERENCES employee(employee_id));
 ALTER TABLE employee_roles ADD ( CONSTRAINT employee_roles_fk1 FOREIGN KEY(roles_id) REFERENCES roles(roles_id));
 
+CREATE OR REPLACE TRIGGER delete_roles
+BEFORE DELETE ON roles
+FOR EACH ROW 
+WHEN(OLD.roles_id IS NOT NULL)
+
+DECLARE
+	var_id varchar(10);
+BEGIN
+	var_id :=:OLD.roles_id;
+
+	DELETE FROM employee_roles
+	WHERE roles_id= var_id;
+END delete_roles;
+/
 
 CREATE TABLE load (
 	load_id int NOT NULL,
@@ -148,8 +165,9 @@ CREATE TABLE load (
 );
 ALTER TABLE load ADD ( CONSTRAINT load_fk_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id));
 ALTER TABLE load ADD ( CONSTRAINT load_fk_trailer  FOREIGN KEY (trailer_id) REFERENCES trailer(trailer_id));
-
 ALTER TABLE load ADD ( CONSTRAINT load_pk PRIMARY KEY (load_id));
+ALTER TABLE load ADD ( CONSTRAINT load_type_check CHECK(type IN ('Wheat', 'Barley', 'Rye', 'Oats', 'Corn'));
+
 CREATE SEQUENCE load_seq 
 	START WITH 1;
 
@@ -167,9 +185,9 @@ CREATE TABLE machine(
 	machine_id int NOT NULL,
 	make varchar(255) NOT NULL,
 	model varchar(255) NOT NULL,
-	year int NOT NULL,
-	county varchar(2) NOT NULL,
-	reg_number int NOT NULL
+	year int,
+	county varchar(2),
+	reg_number int
 );
 ALTER TABLE machine ADD ( CONSTRAINT machine_pk PRIMARY KEY (machine_id));
 
@@ -183,6 +201,19 @@ BEGIN
 	INTO :new.machine_id
 	FROM dual;
 END;
+/
+
+CREATE OR REPLACE TRIGGER delete_farmer
+BEFORE DELETE ON farmer
+FOR EACH ROW
+WHEN(OLD.farmer_id IS NOT NULL)
+DECLARE 
+	temp_id INTEGER;
+BEGIN 
+	temp_id :=: OLD.farmer_id;
+	DELETE FROM trailer AND load
+	WHERE farmer_id=temp_id;
+END delete_farmer;
 /
 
 CREATE TABLE usesOfMachines(
@@ -211,8 +242,6 @@ ALTER TABLE machine_use ADD ( CONSTRAINT machine_use_fk FOREIGN KEY (machine_id)
 ALTER TABLE machine_use ADD ( CONSTRAINT machine_use_fk2 FOREIGN KEY (use_id) REFERENCES usesOfMachines(use_id));
 ALTER TABLE machine_use ADD ( CONSTRAINT machine_use_pk PRIMARY KEY (use_id, machine_id));
 
-
-
 INSERT INTO usesOfMachines (use) VALUES ('Load');
 INSERT INTO usesOfMachines (use) VALUES ('Unload');
 INSERT INTO usesOfMachines (use) VALUES ('Clean');
@@ -232,6 +261,8 @@ INSERT INTO farmer (name, streetName, town, county)
 	VALUES ('David Kelly', 'Grange', 'Athy', 'Kildare' );
 INSERT INTO farmer (name, streetName, town, county)
 	VALUES ('Joey Fitzpatrick', 'King Road', 'Athy', 'Kildare' );
+INSERT INTO farmer (name, streetName, town, county)
+	VALUES ('John Froome', 'Sky Road', 'Athy', 'Kildare' );
 
 INSERT INTO trailer (colour, make, farmer_id) 
 	VALUES ('Red', 'RossMore', 3);
@@ -245,6 +276,8 @@ INSERT INTO trailer (colour, make, farmer_id)
 	VALUES ('Green', 'IforWilliams', 3);
 INSERT INTO trailer (colour, make, farmer_id) 
 	VALUES ('Silver', 'Blackstome', 2);
+INSERT INTO trailer (colour, make, farmer_id) 
+	VALUES ('Black', 'Blackstome', 5);
 
 
 INSERT INTO employee (name, streetName, town,county,salary, phoneNumber, branch_id)
@@ -255,6 +288,9 @@ INSERT INTO employee (name, streetName, town,county,salary, phoneNumber, branch_
 	VALUES ('Bobby Pyrne', 'Tight Bend Street', 'Kildangan', 'Kildare', 52000, 089531245, 2);
 INSERT INTO employee (name, streetName, town,county,salary, phoneNumber, branch_id)
 	VALUES ('Simon Blanch', 'Tall Door Lane', 'Newbridge', 'Kildare', 62000, 08512345678, 2);
+INSERT INTO employee (name, streetName, town,county,salary, phoneNumber, branch_id)
+	VALUES ('John Croc', 'Silver Hedge Row', 'Kildangan', 'Kildare', 71000, 0851144785, 1);
+
 
 INSERT INTO load (type, weight, dateUnloaded, employee_id, trailer_id)
 	VALUES ('Wheat', 12000, (TO_DATE('2016/05/03', 'yyyy/mm/dd')), 1, 3);
@@ -264,6 +300,8 @@ INSERT INTO load (type, weight, dateUnloaded, employee_id, trailer_id)
 	VALUES ('Barley', 10000, (TO_DATE('2016/05/03', 'yyyy/mm/dd')), 3, 4);
 INSERT INTO load (type, weight, dateUnloaded, employee_id, trailer_id)
 	VALUES ('Rye', 7000, (TO_DATE('2016/05/03', 'yyyy/mm/dd')), 4, 1);
+INSERT INTO load (type, weight, dateUnloaded, employee_id, trailer_id)
+	VALUES ('Rye', 8000, (TO_DATE('2016/04/03', 'yyyy/mm/dd')), 3, 1);
 
 INSERT INTO machine (make, model, year, county, reg_number)
 	VALUES ('Case IH', 'LM105', 161, 'KE', 4528);
@@ -273,6 +311,9 @@ INSERT INTO machine (make, model, year, county, reg_number)
 	VALUES ('Case IH', 'PUMA 165', 12, 'KE', 196);
 INSERT INTO machine (make, model, year, county, reg_number)
 	VALUES ('Case IH', 'Farmall', 06, 'KE', 9541);
+INSERT INTO machine (make, model, year, county, reg_number)
+	VALUES ('McHale', 'SweepMax 200', 161, 'RN', 5220);
+
 
 INSERT INTO machine_use (use_id, machine_id)
 	VALUES (1,3);
@@ -291,7 +332,8 @@ INSERT INTO roles(role)
 	VALUES ('Driver');
 INSERT INTO roles(role)
 	VALUES ('Load Attendent');
-
+INSERT INTO roles(role)
+	VALUES ('Cleaner');
 INSERT INTO employee_roles(roles_id, employee_id)
 	VALUES (1,1);
 INSERT INTO employee_roles(roles_id, employee_id)
@@ -301,6 +343,22 @@ INSERT INTO employee_roles(roles_id, employee_id)
 INSERT INTO employee_roles(roles_id, employee_id)
 	VALUES (4,2);
 INSERT INTO employee_roles(roles_id, employee_id)
-	VALUES (1,3);
-	
+	VALUES (3,3);
+INSERT INTO employee_roles(roles_id, employee_id)
+	VALUES (5,4);
+INSERT INTO employee_roles(roles_id, employee_id)
+	VALUES (1,5);
 
+
+DECLARE
+	salary_temp INTEGER;
+	employee_id_temp INTEGER := 3;
+BEGIN
+	SELECT salary
+	INTO salary_temp
+	FROM employee
+	WHERE employee_id= employee_id_temp;
+	dbms_output.put_line(salary_temp);
+	dbms_output.put_line('Employee Number: '|| employee_id_temp || ' earned ' || salary_temp);
+END;
+/
